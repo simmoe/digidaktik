7a537f78295aa7139d0b2bf925eb8053af7f5f54
Simon Moe <simmoe@gmail.com>
2017-08-14 13:07:35 +0200

    Rettet i snake spil og rettet lidt i variable med mus

--- CodingChallenges/CC_03_Snake_game_p5.js/sketch.js
+++ CodingChallenges/CC_03_Snake_game_p5.js/sketch.js
@@ -1,16 +1,33 @@
 var s;
-
+var scl = 20;
+var food;
+var forward = "";
 function setup(){
     createCanvas(400,400);
+    frameRate(10);
+    pickFoodLocation();
     s = new Snake();
 }
 
 function draw(){
     background(51);
+    s.death();
     s.update();
     s.show();
+    //Tegn food vektoren
+    fill(255,0,100);
+    rect(food.x,food.y,scl,scl);
+    if(s.eat(food)){
+        pickFoodLocation();   
+    }
 }
 
+function pickFoodLocation(){
+    var cols = floor(width/scl);
+    var rows = floor(height/scl);
+    food = createVector(floor(random(cols)),floor(random(rows)));
+    food.mult(scl);
+}
 function keyPressed(){
     if(keyCode == UP_ARROW){
         s.dir(0,-1);
