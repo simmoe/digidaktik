73bd0ef41cd8a4cc37b42c43c185bf0e9f21d04f
Simon Moe <simmoe@gmail.com>
2017-09-07 15:32:14 +0200

    Ændret filnavne og opgaver

--- p5/5.4_objekter_med_funktioner/sketch.js
+++ p5/5.4_objekter_med_funktioner/sketch.js
@@ -1,15 +1,19 @@
+//Vi har set noget lignende før, men som du ser er der kommet en funktion "ind" i objektet - move, som kan flytte objektet. 
+
 var bubble = {
   x: 300,
   y: 200,
+  //funktionen display sørger for at vise boblen
   display: function() {
     stroke(255);
     strokeWeight(4);
     noFill();
     ellipse(this.x, this.y, 24, 24);
   },
+  //funktionen move bruger objektets egne x og y variable til at flytte lidt rundt på objektet  
   move: function() {
-    this.x = this.x + random(-1, 1);
-    this.y = this.y + random(-1, 1);
+    this.x = this.x + random(-1,1);
+    this.y = this.y + random(-1,1);
   }
 }
 
@@ -22,3 +26,14 @@ function draw() {
   bubble.move();
   bubble.display();
 }
+
+
+
+/*
+OPGAVER 
+
+1  Prøv at lave to parametre i display funktionen, som sætter  farver på cirklens  
+
+2  prøv at kigge i de tidligere eksempler - og se om du kan implementere funktionen bounce i objektet
+
+*/
