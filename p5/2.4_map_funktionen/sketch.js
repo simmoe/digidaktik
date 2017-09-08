d35bf79ecc6a8326907d58e8bc058ab4adb7ea48
Simon Moe <simmoe@gmail.com>
2017-09-08 12:34:10 +0200

    Opgaver til constructor funktionen

--- p5/2.4_map_funktionen/sketch.js
+++ p5/2.4_map_funktionen/sketch.js
@@ -10,15 +10,13 @@ function setup() {
 }
 
 function draw() {
-  // background
-  //col = mouseX;
-  //col = map(mouseX, 0, 600, 0, 255);
-  r = map(mouseX, 0, 600, 0, 255);
-  b = map(mouseX, 0, 600, 255, 0);
+  r = map(mouseX, 0, width, 0, 255);
+  b = map(mouseX, 0, width, 255, 0);
   // background(col);
-  background(r, 0, b);
+  background(r, b, 0);
   
   // ellipse
+  noStroke();
   fill(250, 118, 222);
   ellipse(mouseX, 200, 64, 64);
 }
