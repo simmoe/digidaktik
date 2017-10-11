42eca6e63273884cec395303ca06aa8b0a635ced
Simon Moe <simmoe@gmail.com>
2017-10-11 11:33:35 +0200

    opgaveløsninger

--- html - javascript - css/3_javscript/javascript.js
+++ html - javascript - css/3_javscript/javascript.js
@@ -1,7 +1,8 @@
-function nextQuestion(number){
+function nextQ(number){
     var str = "question-" + number;
     if(document.getElementById(str)){
         document.getElementById("main").innerHTML = document.getElementById(str).innerHTML; 
+        number = this.number;
     }else{
         alert("Kan ikke finde et element med dette id: " + str);
     }
