8533be85856f094f94c558d556be214c3f71329f
Simon Moe <simmoe@gmail.com>
2017-10-12 17:13:34 +0200

    added

---
+++ Unity/DestroySphere.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySphere : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.CompareTag ("projectile")) {
+			this.gameObject.SetActive (false);
+		}
+	}
+
+}
