d5eb52bae7fa09f0e74f18374af68ccc66936d86
LFalch <lucas@wasd.dk>
2017-11-18 00:34:02 +0100

    Lad være at `rm`e og tjek ordentligt efter fejl

--- update.sh
+++ update.sh
@@ -4,16 +4,15 @@ shopt -s globstar
 # Tjek https://github.com/processing/p5.js/releases/latest for at se den
 # nyeste version og giv den som argument
 curl -OL https://github.com/processing/p5.js/releases/download/$1/p5.zip
+unzip p5.zip -d libs
 if [[ $? -eq 0 ]]; then
-    unzip p5.zip -d libs
     for f in **/**/libraries; do
-        rm "$f"/*.js
         cp libs/p5/addons/p5.dom.js "$f"
         cp libs/p5/addons/p5.sound.js "$f"
         cp libs/p5/p5.js "$f"
     done
-    rm p5.zip
     rm -r libs
 else
-    echo "Fejl i at hente p5. Findes versionen ($1)?"
+    echo "Fejl i at hente p5 med versionen '$1'! Findes den?"
 fi
+rm p5.zip
