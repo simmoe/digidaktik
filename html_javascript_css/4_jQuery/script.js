b595e48026d69eee44f16d9df3112ed898371639
Simon Moe <simmoe@gmail.com>
2017-11-24 09:05:57 +0100

    opgaver i jquery

--- html_javascript_css/4_jQuery/script.js
+++ html_javascript_css/4_jQuery/script.js
@@ -1,86 +1,61 @@
 
 
-$('header').click(
+$('h1').click(
     function(){
-        $('#title').fadeOut();   
+        $('h1').fadeOut();   
     }
 );
 
-$("#klik").click(
+$("#knap1").click(
     function(){
-        $('.hidden').toggle();
+
     }
 ); 
 
-$("#skift").click(
+$("#knap2").click(
     function(){
-        var r = Math.floor((Math.random() * 255) + 1);
-        var g = Math.floor((Math.random() * 255) + 1);
-        var b = Math.floor((Math.random() * 255) + 1);
-        
-        $('p').css("color", "rgb(" + r + ", " + g + ", " + b + ")");
-    }
-); 
 
-$("#tekst").click(
-    function(){
-        alert($('#secret').html());
-    }
-); 
-
-$("#foot").click(
-    function(){
-        $('#panel').slideToggle();
-    }
-); 
-
-$("#margin").click(
-    function(){
-        $('body').css({"width":"80%", "margin":"auto"});
-    }
-); 
-
-$("#inp").click(
-    function(){
-        alert($('#email').val());
-    }
-); 
-
-$("#udefra").click(
-    function(){
-        $('#external').load("tekst.txt");
-    }
-); 
-
-$("#udefra").click(
-    function(){
-        $('#external').load("tekst.txt");
     }
 ); 
 
 
-$("#select").change(
+$("#knap3").click(
     function(){
-        alert($("#select").val());
+
     }
-);
+); 
 
 
 /*
 OPGAVER
 
-Brug funktionen $('header').click() til at fade overskriften ("jQuery") ud på samme måde som den anden
+Bemærk hvordan funktionen $('h1').click() fader overskrifter ud i dokumentet - altså: når man klikker på et (hvilket som helst) h1 element, kaldes funktionen.  
 https://www.w3schools.com/jquery/jquery_fade.asp
 
-Prøv at gøre sådan så afsnittet <p class="hidden"> bliver vist, når man klikker på knappen
+Lad os prøve at sørge for at det kun er den øverste overskrift man kan klikke på og fade ud. Brug dens id: #title
+
+Kan du i stedet få alle p-elementer til at fade ud, når man klikker på overskriften?
+
+Brug metoden .css() til at sætte farven på overskriften, når man klikker på den
+https://www.w3schools.com/jquery/jquery_css.asp
+
+Lad os prøve at animere elementet. Brug metoden animate til at sætte en ny højde på overskrifter:
+https://www.w3schools.com/jquery/jquery_animate.asp
+
+Se alle animationer her:
+https://www.w3schools.com/jquery/eff_animate.asp
+
+- - - - - - - -
+
+Prøv at gøre sådan så afsnittet <p class="hidden"> bliver vist, når man klikker på knappen "Vis skjult"
 https://www.w3schools.com/jquery/eff_show.asp
 
-Kan du også sørger for at den fader langsom ind?
+Kan du også sørge for at afsnittet fader langsomt ind? (læs ovenstående link i manualen)
 
-Det ville være bedre hvis den blev skjult når man klikker igen. Kan du ordne det?
+Det ville være bedre hvis afsnittet blev skjult igen, når man klikker en gang til. Kan du ordne det?
 https://www.w3schools.com/jquery/eff_toggle.asp
 
-Kan du få hele siden til at fade ud når man klikker på knappen?
+Kan du få hele siden til at fade ud når man klikker på knappen "skjul siden"?
 
 Kan du få skriftfarven på alle p-elementer til at skifte når man klikker på knappen "Skift farve"? 
 https://www.w3schools.com/jquery/jquery_css.asp
@@ -89,12 +64,9 @@ https://www.w3schools.com/jquery/jquery_css.asp
 
 Lidt sværere opgaver...
 
-Måske kan du også gøre sådan så der skiftes til en tilfældig farve når man klikker på knappen farve? Her skal du bruge rgb(20,30,40) og så kam du oprette et tilfældigt nummer mellem 0 og 255 med metoden Math.random
-https://www.w3schools.com/jsref/jsref_random.asp
+Kan du lave en ny knap i index.html som du giver et id og en tekst. Og kan du derefter lave en funktion her i scriptet, som gør at knappen får dokumentets body til at have width:80% og margin:auto?
 
-Kan du lave en knap som får dokumentets body til at have width:80% og margin:auto?
-
-Lav en anden knap, "input", som laver en alert med teksten i input-feltet, når man klikker på den
+Lav en anden knap, "input", som laver en alert med den tekst som står i input-feltet..
 https://www.w3schools.com/jquery/html_val.asp
 
 Kan du lave en funktion, som laver en alert når man har valgt noget i dropdown boksen?
@@ -104,11 +76,22 @@ https://api.jquery.com/change/
 
 Halvsvære opgaver...
 
+I index.html er der et textarea med id "brugerinput" og en knap under med id "opret". Nedenfor er der et afsnit med id "brugertekst". Kan du få værdien fra textarea ind i afsnittet når man trykker på knappen?
+https://www.w3schools.com/jquery/html_append.asp
+https://www.w3schools.com/jquery/html_val.asp
+
 Lad os så prøve at gå et skridt videre. Med jQuery og AJAX kan man hente data ind udefra. 
-Se om du kan loade teksten fra dokumentet tekst.txt ind i <p id="external">
+Se om du kan loade teksten fra dokumentet tekst.txt ind i dette html. Hvis du kiggr i index.html finder du et afsnit som hedder <p id="external"> - brug dette id til at kalde load metoden:
 https://www.w3schools.com/jquery/jquery_ajax_load.asp
 
+- - - - - - - -
 
+Og et par ..sværere opgaver
+
+Måske kan du også gøre sådan så der skiftes til en tilfældig farve når man klikker på knappen farve? Her skal du bruge rgb(20,30,40) og så kam du oprette et tilfældigt nummer mellem 0 og 255 med metoden Math.random
+https://www.w3schools.com/jsref/jsref_random.asp
+
+Kan du lave dropdown boksen så man vælger mellem to forskellige html sider - og bliver sendt videre til dem når man vælger?
 
 */
 
