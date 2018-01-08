a407de11a014971bb955dd4360c5080b19f365ee
Simon Moe <simmoe@gmail.com>
2018-01-08 12:56:19 +0100

    opgaver til variable

--- p5_basic_programming/3_funktioner/sketch.js
+++ p5_basic_programming/3_funktioner/sketch.js
@@ -41,12 +41,18 @@ function bounce () {
 /*
 OPGAVER
 
-Start med funktionen display(). Hvad skal der til for at kuglen bliver vist? 
+Start med funktionen display(). Hvad skal der til for at kuglen bliver vist? Du skal bruge funktionen ellipse ligesom sidst, men husk at du skal bruge cirklens variable: ball.x, ball.y etc..
 
-Dernæst funktionen move() - hvad skal der til for at kuglen kan bevæge sig? Du skal tænke sadan, at funktionen draw() bliver kaldt hele tiden (60 gange hver sekund). Og hvad gør draw så 60 gange i sekundet? Jo, den kalder funtionerne display, move og bounce...   
+Dernæst funktionen move() - hvad skal der til for at kuglen kan bevæge sig? Du skal tænke sadan, at funktionen draw() bliver kaldt hele tiden (60 gange hver sekund). Og hvad gør draw så 60 gange i sekundet? Jo, den kalder funtionerne display, move og bounce... Så hvis du i move() funktionen påvirker cirklens x- og y-værdier, så flytter den sig..
 
 Tag til sidst funktionen bounce() - hvad skal der til for at kuglen ikke kommer til at bevæger sig udenfor skærmens højde og bredde? 
 
 Tip: if(ball.x > width){...}
+
+- - - -
+
+Sværere opgaver
+
+Se om du kan bruge funktionen map() til at sætte cirklens alpha værdi. Funktionen fill(r, g, b) har en fjerde parameter - a - som sætter fyldfarvens transparens. Hvis du for eksempel skriver fill(255, 0, 0, 127), farver du noget helt rødt, med en transparens på 50%. Funktionen map() gør det muligt at flutte en variabels værdi fra én skala til en anden. Går variablen x for eksempel fra 0-600, kan du bruge map() til at omregne dens værdi til en skala fra 0-255. Se: https://p5js.org/reference/#/p5/map
 
 */
