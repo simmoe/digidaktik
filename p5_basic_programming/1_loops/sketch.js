f6c709d64b96a0062d85d549df1c4c82e1afe1d2
Simon Moe <simmoe@gmail.com>
2018-02-01 08:52:12 +0100

    oprettet nye basic programming mapper

--- p5_basic_programming/1_loops/sketch.js
+++ p5_basic_programming/1_loops/sketch.js
@@ -9,13 +9,13 @@ function setup() {
 //Draw bliver kaldt hele tiden mens programmet kører
 function draw() {
     background(255,204,0);
-    fill(0,255,0);
+    fill("green");
     noStroke();
     textSize(12);
 
     //Her er så en løkke. Den skriver tallene fra 0-19 på skærmen - men den skriver dem alle sammen i koordinatet (20,40)
-    for(int = 0; int < 20; int++){
-        text(int, 20, 40);
+    for(var i = 0; i < 30; i++){
+        text(i, 20 * i, 20);
     }
 }
 
@@ -24,9 +24,9 @@ OPGAVER
 
 Det er lidt irriterende at tallene bliver skrevet oven i hinanden. Det skyldes funktionen text(). Denne funktion fungerer sådan at man giver den tre parametre: hvad den skal skrive, samt et x-koordinat og et y-koordnat, som bestemmer hvor på skærmen det bliver skrevet. 
 
-Kan du bruge variablen int til at gøre x-koordinatet lidt større hver gang, så tallene bliver skrevet hver for sig? 
+Kan du bruge variablen i til at gøre x-koordinatet lidt større hver gang, så tallene bliver skrevet hver for sig? 
 
-Funktionen ellipse(x, y, width, height) tegner en cirkel på skærmen. Kan du bruge funktionen til at tegne en pæn rund cirkel under hver af tallene?
+Funktionen ellipse(x, y, width, height) tegner en cirkel på skærmen. Kan du bruge funktionen til at tegne en rund cirkel under hver af tallene?
 https://p5js.org/reference/#/p5/ellipse
 
 Funktionen fill(r, g, b) sætter den farve man tegner med på skærmen. Måske kan du skiftevis sætte farven hver gang der tegnes enten et tal eller en cirkel, så tal og cirkler får forskellig farve?
@@ -38,7 +38,13 @@ Funktionen rect(x, y, width, height) tegner en firkant på skærmen. Kan du brug
 Funktionen fill(r, g, b) har faktisk en parameter til - alpha. Som er et tal mellem 0 og 255 som sætter hvor transparent farven skal være. Kan du sætte tallenes farve, så de gradvist bliver tydeligere, når int nærmer sig 20? 
 
 - - - - 
+
 Sværere opgaver 
+
+Man kan sagtens lave en for-løkke inden i en anden for løkke. Altså fx:
+for(var e=0; e < 20; e++){...}
+
+Se om du kan skrive tallene fra 0-30 hen ad skærmen og tallene fra 0-20 ned ad skærmen med to for løkker
 
 Kan du gøre det omvendt så de langsomt fader ud? Det er lidt vanskeligt, for her skal du bruge variablen int (som jo tæller fra 0 til 19) til at producere et tal som begynder med 255 og gradvist kommer nærmere nul.
 
