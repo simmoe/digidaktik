3280e4aa7595b03febd2ed31c7d2fe23f8da8b4f
Simon Moe <simmoe@gmail.com>
2018-02-19 12:22:13 +0100

    smårettelser

--- p5_basic_programming/x_jump_ball/ball.js
+++ p5_basic_programming/x_jump_ball/ball.js
@@ -34,26 +34,26 @@ Og lad os bare starte med metoden update(); Hvad skal der til for at bolden fald
 
 Jo, så må vi tilføje noget til boldens y-værdi. Og for at gøre det rigtigt, kan vi lige så godt starte med at lave en variabel. Lad os kalde den gravity. 
 
-Opgave 1: Lav en variabel this.gravity = 0.6, på samme måde som x og y variablene er defineret. Brug variablen til at sætte boldens y-værdi i funktionen up();
+Opgave 1: Lav en variabel this.gravity = 0.6, på samme måde som x og y variablene er defineret. Brug variablen til at sætte boldens y-værdi i funktionen update();
 
 Men for at følge Newton bare en lille smule, hvor vi husker at der både er tyngdekraft og hastighed, laver vi en variabel til - velocity (hastighed). Som vi så hele tiden lægger sammen med med gravity, hvorefter vi lægger VELOCITY til y-værdien. (tyngdekraften gør at boldens hastighed bliver større jo længere den falder) 
 
-Opgave 2: lav en ny variabel, this.velocity = 0, som adderes med gravity og derefter opskriver boldens y-værdi.
+Opgave 2: lav en ny variabel, this.velocity = 0, som først adderes med gravity og derefter opskriver boldens y-værdi (i stedet for gravity).
 
 Nu forsvinder bolden pænt nedad, MEN når den rammer bunden, forsvinder den ud af skærmen. Vi må hellere lave en betingelse i funktionen update() som sætter en grænse for hvad boldens y-værdi kan være. 
 
-Opgave 3: Lav en betingelse (if(){}) i update(), som sørger for at bolden ikke kan ryge ud af skærmens bund.
+Opgave 3: Lav en betingelse (if(){}) i update(), som sørger for at bolden ikke kan ryge ud af skærmens bund. Brug operatoren >= så du spørger om boldens y-værdi er blevet større end eller lig med højden  
 
 Nu mangler vi ligesom noget vi kan skubbe til bolden med. Lad os lave en ny variabel, lift, som vi kan bruge til hele tiden at få bolden til at hoppe opad, når brugeren trykker på mellemrumstasten. 
 
-Opgave 4: lav en variabel, lift, og brug den til at sende bolden opad i update();
+Opgave 4: lav en variabel, lift, og brug den til at sende bolden opad i update(); Husk ikke at påvirke boldens y-værdi direkte - brug velocity!
 
 Vi har lidt glemt noget luftmodstand. Luftmodstand vil påvirke boldens hastighed i negativ retning med en eller anden konstant. Hastighed * luftmodstand. 
 
-Opgave 5: Kan du oprette en variabel, friction, som hele tiden påvirker hastigheden lidt?
+Opgave 5: Kan du oprette en variabel, friction, som hele tiden påvirker hastigheden lidt? (gang velocity med en eller anden friction < 1, før du lægger den til boldens y-værdi)
 
 Det sidste der er lidt kedeligt er at bolden ikke hopper når den rammer bunden. Kan du bruge hvad du har lært indtil nu, for at få den til det?
 
-tip: Du skal se på den betingelse hvor bolden rammer bunden. Hidtil har vi sat boldens y værdi til 0. Bum. Det er lidt primitivt. Prøv samtidig at påvirke boldens hastighed til at være negativ.  
+tip: Du skal se på den betingelse hvor bolden rammer bunden. Hidtil har vi sat boldens y værdi til 0 hvis den er større end eller lig med højden. Bum. Det er lidt primitivt. Prøv samtidig at påvirke boldens hastighed til at være negativ.  
 
 */
