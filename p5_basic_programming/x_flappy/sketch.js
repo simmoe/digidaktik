e2b0b3f16e141d93507c84c5269ab716caf7d0d0
Simon Moe <simmoe@gmail.com>
2018-03-13 09:08:40 +0100

    Rettet opgaver

--- p5_basic_programming/x_flappy/sketch.js
+++ p5_basic_programming/x_flappy/sketch.js
@@ -24,10 +24,7 @@ function draw() {
     background(0);
     
     
-    if(frameCount % 60 == 0){
-        points +=20;
-    }
-    pointCounter.html(points);
+  pointCounter.html(points);
 
   for (var i = pipes.length-1; i >= 0; i--) {
     pipes[i].show();
@@ -62,12 +59,14 @@ function keyPressed() {
 /*
 Opgaver
 
-Vi vil gerne lave spillet sådan, at det kan spilles på en mobiltelefon. Derfor er vi nødt til at have en knap, som kan få bolden til at hoppe. 
+Vi vil gerne lave spillet sådan, at det kan spilles på en mobiltelefon. Derfor må vi have en knap, som kan få bolden til at hoppe. 
 
 Se hvordan man laver en tryk-knap her: 
 https://p5js.org/examples/dom-input-and-button.html
 
-Lav en knap på et fornuftigt sted på skærmen (helst udenfor selve spillet), og få bolden til at hoppe når man klikker på den. 
+Lav en knap og få bolden til at hoppe når man klikker på den. 
+
+Giv knappen en klasse - på samme måde som pointCounter ovenfor. 
 
 Gå så ind i skitsens stylesheet og se om du kan få knappen til at være meget mere iøjnefaldende. Se fx 
 https://www.w3schools.com/css/css3_buttons.asp
@@ -75,54 +74,80 @@ https://www.w3schools.com/css/css3_shadows.asp
 
 _ _ _ _ _ _ _ _
 
-Point. Vi vil gerne have pointtælleren øverst på skærmen til at vise nogle point. Der skal gives 20 point i sekundet, men trækkes 80 fra, hvis man rammer en væg.
+Point. Vi vil gerne have pointtælleren øverst på skærmen til at vise nogle point. Der kan for eksempel gives 20 point i sekundet, og trækkes 50 fra, hvis man rammer en væg. Eller lignende. 
 
 Se først om du kan oprette en variabel: points, og skrive den i pointfeltet i stedet for teksten "nul".
 
 Brug variablen frameCount og MODULO operatoren til at sørge for at der bliver uddelt nøjagtig 20 point i sekundet.
 
-Tip: du lave en betingelse som spørger om frameCount % 20 == 0. Inden i betingelsen skal du så lægge 20 til points variablen. 
+Tip: du lave en betingelse som spørger om frameCount % 20 == 0. Inden i betingelsen skal du så lægge 20 til points variablen "points". 
 
 Kan du så også sørge for, at der bliver trukket nogle point fra, hver gang flappy støder ind i en bjælke?
 
-Tip: Se i koden hvor der står:             console.log("Bad points"); 
+Tip: Se i koden hvor der står:             
+console.log("Bad points"); 
 
 _ _ _ _ _ _ _ _
 
 Hvis pointene kommer under nul, skal spillet slutte. Lav først en funktion der hedder gameOver().
 
-Tip: 
+Tip - sådan skriver man en ny funktion:
 
 function gameOver(){
 
 }
 
-Kald så den nye funktion fra draw(), hvis points er mindre end nul. 
+Nu skal den nye funktion kaldes fra draw(), hvis "points" er blevet mindre end nul. Her skal du tænke sådan, at ALT det der sker inde i draw() skal kun ske, hvis "points > 0". 
+
+Det kan løses med en if(){}else{} betingelse. 
 
 Tip: Lav en betingelse som spørger om points er mindre end nul..
 
-Lav derefter en variabel: gameOver - som du sætter til false fra spillets start. Alt inden i draw metoden skal KUN udføres hvis variablen gameOver er false! Det vil altså sige, at hvis gameOver bliver true - så sker der ingenting i draw() længere. 
+if(points < 0){
+    //Kald funktionen gameOver
+}else{
+    //Gør alt det der skal ske inde i draw();
+}
 
-Tip: lav en betingelse som det allerførste i draw() som spørger om gameOver er true - 
 
-Nu vil vi gerne have en tæller mere i øverste højre side af skærmen, som viser hvor mange sekunder man har klaret. 
+Nu vil vi gerne have en tæller mere i øverste højre side af skærmen, som viser hvor mange sekunder man har spillet. 
 
 Kan du oprette et felt mage til pointCounter, som i stedet hedder secCounter? 
 
 Kan du give elementet en anden klasse end "points" og i stylesheetet sørge for at den kommer over i højre side?
 
 Kan du bruge den faste variabel "frameCount" til at sætte html'en i secCounter til sekunderne?
-(tip: frameCount skal divideres med 60 for at tælle sekunder - men resultatet skal transformeres med metoden parseInt(decimal) , for at blive til heltal)
+
+(tip: frameCount kan divideres med 60 for at tælle sekunder - men resultatet skal transformeres med metoden parseInt(decimal) , for at blive til heltal). Du kan også bruge modulo operatoren igen. 
 
 _ _ _ _ _ _ _ _
 
-Afsluttende, rimelig svær opgave
+Afsluttende, lidt sværere opgave
 
 Kan du sørge for at spillet slutter enten hvis der går tyve sekunder - eller hvis man kommer under 0 i point. Og at der dermed kommer en stor tekst frem på skærmen, hvor der står hhv "Game Over" eller "The End"?
 
-Kan du også sørge for, at der kommer en ny knap frem som viser hvor mange point man fik?
+Kan du også så sørge for, at der kommer en ny knap frem som viser hvor mange point man fik?
 
 Og kan du sørge for at man kan klikke på knappen og begynde forfra? (tip: brug location.reload() - https://www.w3schools.com/jsref/met_loc_reload.asp )
+
+_ _ _ _ _ _ _ _
+
+Når du har nået ovenstående, kan du begynde at tage stilling til hvordan dit spil ser ud. 
+
+Først kan du bruge de såkaldte "Gestaltlove" til at vurdere den visuelle brugergrænseflade
+
+Læs her og vurder dit eget design: http://www.nielsgamborg.dk/?p=gestaltlovene
+
+Er der noget du skal lave om?
+
+Læs dernæst dette kapitel i i-bogen informatik, giver en introduktino til hvordan man kan tænke fagligt over hvilke valg man træffer i et design:
+
+https://informatik.systime.dk/index.php?id=1100
+
+Lever dit spil op til kravene om et "brugervenligt it-system"?
+
+Tegn et flow-diagram over spillet...
+
 
 
 */
