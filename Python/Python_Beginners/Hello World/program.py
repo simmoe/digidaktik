1d935dab951f76baa7be5257525689d117f922ef
simmoe <simmoe@gmail.com>
2018-10-01 15:41:52 +0200

    første forsøg med en kommenteret stump

---
+++ Python/Python_Beginners/Hello World/program.py
@@ -0,0 +1,21 @@
+#Source kode i python begynder ofte med nørdede bogstavtegninger
+print('--------------------------------')
+print('          HELLO Pipi')
+print('--------------------------------')
+print()
+
+# variabler sættes og initieres samtidig
+name = 'Tommy'
+
+# de kan manipuleres med det samme
+age = 10
+age +=1
+hobbies = ['running', 'playing', 'steam-motor']
+
+# Sammensatte ord adskilles med underscore
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
+
+print('My name is ' + name)
