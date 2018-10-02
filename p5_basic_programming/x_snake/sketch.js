67599543fc3cf2b678efef6e738dd51740bbc298
simmoe <simmoe@gmail.com>
2018-10-02 10:08:14 +0200

    ingen rettelser

--- p5_basic_programming/x_snake/sketch.js
+++ p5_basic_programming/x_snake/sketch.js
@@ -9,47 +9,55 @@ var scl = 40;
 var food;
 
 function setup() {
-  createCanvas(600, 600);
-  s = new Snake();
-  frameRate(10);
-  pickLocation();
+    window.addEventListener("keydown", function (e) {
+        // space and arrow keys
+        if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+        }
+    }, false);
+    createCanvas(600, 600);
+    s = new Snake();
+    frameRate(10);
+    pickLocation();
 }
 
 function pickLocation() {
-  var cols = floor(width/scl);
-  var rows = floor(height/scl);
-  food = createVector(floor(random(cols)), floor(random(rows)));
-  food.mult(scl);
+    var cols = floor(width / scl);
+    var rows = floor(height / scl);
+    food = createVector(floor(random(cols)), floor(random(rows)));
+    food.mult(scl);
 }
 
 function mousePressed() {
-  s.total++;
+    s.total++;
 }
 
 function draw() {
-  background(51);
-
-  if (s.eat(food)) {
-    pickLocation();
-  }
-  s.death();
-  s.update();
-  s.show();
+    background(51);
+    text("Test mus", 40,40);
 
 
-  fill(255, 0, 100);
-  rect(food.x, food.y, scl, scl);
+    if (s.eat(food)) {
+        pickLocation();
+    }
+    s.death();
+    s.update();
+    s.show();
+
+
+    fill(255, 0, 100);
+    rect(food.x, food.y, scl, scl);
 }
 
 
 function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    s.dir(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
-    s.dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1, 0);
-  }
+    if (keyCode === UP_ARROW) {
+        s.dir(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+        s.dir(0, 1);
+    } else if (keyCode === RIGHT_ARROW) {
+        s.dir(1, 0);
+    } else if (keyCode === LEFT_ARROW) {
+        s.dir(-1, 0);
+    }
 }
