351bf93d29dfbca6db14d48846625984ba0952a2
simmoe <simmoe@gmail.com>
2018-10-11 17:43:24 +0200

    Kørt sockets og sockets done ok

--- sockets/sockets_1/sketch.js
+++ sockets/sockets_1/sketch.js
@@ -1,25 +1,7 @@
 function setup() {
-    createCanvas(400, 400);
-    background(0);
     // Start a socket connection to the server
-    // Some day we would run this server somewhere else
-
-    // We make a named event called 'mouse' and write an
-    // anonymous callback function
 }
 
 function draw() {
-    // Nothing
-}
-
-function mouseDragged() {
-    // Draw some white circles
-    // Send the mouse coordinates
-}
-
-// Function for sending to the socket
-function sendmouse(xpos, ypos) {
-    // We are sending!
-    // Make a little object with  and y
-    // Send that object to the socket
+    // Do something
 }
