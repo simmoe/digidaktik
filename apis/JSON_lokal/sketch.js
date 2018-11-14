8ccd55fd6b171f3181535979cbef99db85b4ae97
simmoe <simmoe@gmail.com>
2018-11-14 21:03:36 +0100

    fjernet lidt kode, lavet lidt opgaver

--- apis/JSON_lokal/sketch.js
+++ apis/JSON_lokal/sketch.js
@@ -20,10 +20,17 @@ function setup() {
     //Så løber vi igennem dem, og udskriver familienavnet
     birds.forEach(element => {
         createElement('h1', element.family);
-        //Og hvis vi også vil udskive fuglene i hver familie
-        var theBirds = element.members;
-        theBirds.forEach(element => {
-            createElement('p', element);
-        });
+        //Og hvis vi også vil udskrive fuglene i hver familie, laver vi en ny variabel med selve fuglene i - og?
     });
 }
+
+/*Opgaver
+
+Prøv at udskrive familien "Penguins" og dens members
+
+prøv at tilføje et link øverst, som henviser til kilden (source)
+
+Find selv et JSON datasæt, fx her: https://github.com/dariusk/corpora/tree/master/data
+Lav en overskuelig liste med indholdet
+
+*/
